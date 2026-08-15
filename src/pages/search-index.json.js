f54cfa2755a74950823750bfb5822cac1e561aee
Tushar raj <rajambujtushar@gmail.com>
2026-08-15 12:32:35 +0530

    Create search-index.json.js

---
+++ src/pages/search-index.json.js
@@ -0,0 +1,15 @@
+import { getCollection } from 'astro:content';
+
+export async function GET() {
+  const posts = await getCollection('blog', ({ data }) => !data.draft);
+  const index = posts.map(post => ({
+    title: post.data.title,
+    description: post.data.description,
+    category: post.data.category,
+    tags: post.data.tags || [],
+    url: `/blog/${post.slug}/`,
+  }));
+  return new Response(JSON.stringify(index), {
+    headers: { 'Content-Type': 'application/json' },
+  });
+}

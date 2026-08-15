bcf35c6c98b7e70d181d316b150ede426674902c
Tushar raj <rajambujtushar@gmail.com>
2026-08-15 12:22:17 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,19 @@
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   site: 'https://aicareerguide.xyz',
-  integrations: [sitemap()],
+  integrations: [mdx(), sitemap(), tailwind()],
+  output: 'static',
+  build: {
+    inlineStylesheets: 'auto',
+  },
+  markdown: {
+    shikiConfig: {
+      theme: 'github-dark',
+      wrap: true,
+    },
+  },
 });

0c2188dee6f8cb7a659c956dc4cfa98b4540e93c
Tushar raj <rajambujtushar@gmail.com>
2026-08-16 09:07:23 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,7 +14,10 @@ export default defineConfig({
   },
   markdown: {
     shikiConfig: {
-      theme: 'github-dark',
+      themes: {
+        light: 'github-light',
+        dark: 'github-dark',
+      },
       wrap: true,
     },
   },
